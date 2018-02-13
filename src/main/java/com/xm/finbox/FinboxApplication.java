5d95090f687a42d9eb2fb9280ae75af8a5d691e7
George Sapountzis <gsapountzis@gmail.com>
2018-02-13 11:58:57 +0200

    Import generated project

---
+++ src/main/java/com/xm/finbox/FinboxApplication.java
@@ -0,0 +1,12 @@
+package com.xm.finbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinboxApplication.class, args);
+	}
+}
